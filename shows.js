5f9e34516f050634866c2b2ec5d4d66a9eece25c
Eric Terry <eric.terry@intouchsol.com>
2020-04-03 17:53:33 -0500

    driving contenders through random javascript

--- shows.js
+++ shows.js
@@ -8,7 +8,7 @@ const request = async () => {
   //console.log(shows);
 
   sortedShows = shows.sort(function(a, b){return 0.5 - Math.random()});
-  console.log(sortedShows);
+  //console.log(sortedShows);
   printSortedShows(sortedShows);
 }
 
@@ -16,14 +16,80 @@ request();
 
 function printSortedShows(sortedShows)
 {
-  var html = "<ol>";
+  var left = "";
+  var right = "";
+  let match = -1;
+  let contenderCount = 0;
+
+  for(i=0; i<32; i++)
+  {
+    if(contenderCount === 0)
+    {
+      match++;
+    }
+    if(i<16)
+    {
+    left += "<div class=\"match match-" + match + "\">";
+    left += "<div class=\"status\">";
+    left += "    <span>Winner: Option X</span>";
+    left += "</div>";
+    left += "<div class=\"contenders\">";
+    left += "    <div class=\"contender\">";
+    left +=      sortedShows[i].name;
+    left += "     <div class=\"dropdown-content\">";
+    left += "            <a href=\"" + sortedShows[i].url + "\" target=\"_blank\">Listen To Theme</a>";
+    left += "            <a href=\"#\">Vote</a>";
+    left += "        </div>";
+    left += "    </div>";
+    i++;
+    left += "    <div class=\"contender\">";
+    left +=      sortedShows[i].name;
+    left += "        <div class=\"dropdown-content\">";
+    left += "            <a href=\"" + sortedShows[i].url + "\" target=\"_blank\">Listen To Theme</a>";
+    left += "            <a href=\"#\">Vote</a>";
+    left += "        </div>";
+    left += "    </div>";
+    left += "</div>";
+    left += "</div>";
+    }
+    if(i === 16) { match = 0; }
+    if(i>=16)
+    {
+    right += "<div class=\"match match-" + match + "\">";
+    right += "<div class=\"status\">";
+    right += "    <span>Winner: Option X</span>";
+    right += "</div>";
+    right += "<div class=\"contenders\">";
+    right += "    <div class=\"contender\">";
+    right +=      sortedShows[i].name;
+    right += "     <div class=\"dropdown-content\">";
+    right += "            <a href=\"" + sortedShows[i].url + "\" target=\"_blank\">Listen To Theme</a>";
+    right += "            <a href=\"#\">Vote</a>";
+    right += "        </div>";
+    right += "    </div>";
+    i++;
+    right += "    <div class=\"contender\">";
+    right +=      sortedShows[i].name;
+    right += "        <div class=\"dropdown-content\">";
+    right += "            <a href=\"" + sortedShows[i].url + "\" target=\"_blank\">Listen To Theme</a>";
+    right += "            <a href=\"#\">Vote</a>";
+    right += "        </div>";
+    right += "    </div>";
+    right += "</div>";
+    right += "</div>";
+    }
+  }
+
+  /*
   for(i=0; i<64; i++)
   {
     html += "<li><a href=\"" + sortedShows[i].url + "\" target=\"_blank\">" + sortedShows[i].name + "</></li>";
   }
   html += "</ol>";
+  */
 
-  document.getElementById("demo").innerHTML = html;
+  document.getElementById("left").innerHTML = left;
+  document.getElementById("right").innerHTML = right;
 }
 
 /*
